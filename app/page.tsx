368934feb09382e176aa0f46b6421c76b0f94b16
ReDay Zarra <redayzarra@gmail.com>
2023-09-05 07:08:14 -0400

    Learning to protect routes from unidentified users

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import styles from './page.module.css'
+import { getServerSession } from "next-auth";
+import styles from "./page.module.css";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      
-    </main>
-  )
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+    return <p>You must be signed in...</p>;
+  }
+
+  return <main className={styles.main}></main>;
 }
