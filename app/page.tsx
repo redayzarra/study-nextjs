625e678ef6e5fbbfc083a034b02d8b1be1b99750
ReDay Zarra <redayzarra@gmail.com>
2023-09-06 11:42:22 -0400

    Setting up loading screen for user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { getServerSession } from "next-auth";
-import styles from "./page.module.css";
-import { redirect } from "next/navigation";
-
-export default async function Home() {
-  const session = await getServerSession();
-
-  if (!session) {
-    redirect("/api/auth/signin");
-    return <p>You must be signed in...</p>;
-  }
-
-  return <main className={styles.main}></main>;
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>To get started, sign up for an account</p>
+    </div>
+  );
 }
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function UserProfile({ params }: Props) {
   const user = await prisma.user.findUnique({ where: { id: params.id } });
-  const { name, bio, image } = user ?? {};
+  const { name, bio, image, id } = user ?? {};
 
   return (
     <div>
